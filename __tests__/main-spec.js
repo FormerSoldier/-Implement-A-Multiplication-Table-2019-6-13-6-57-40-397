993b64246ca220f717986d0b9c896d7bc2bc3350
TYG <793557590@qq.com>
2019-07-10 00:11:48 +0800

    test:add two tests for function isInRange

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -3,3 +3,13 @@ const obj = require('../main');
 it('should return false when call isStartSmallerEqualEnd given 10,1',() => {
     expect(obj.isStartSmallerEqualEnd(10,1)).toBe(false);
 });
+it('should return true when call isStartSmallerEqualEnd given 1,10',() => {
+    expect(obj.isStartSmallerEqualEnd(1,10)).toBe(true);
+});
+
+it('should return false when call isInRange given -1,10', () => {
+    expect(obj.isInRange(-1,10)).toBe(false);
+});
+it('should return true when call isInRange given 10,20', () => {
+    expect(obj.isInRange(10,20)).toBe(true);
+});
