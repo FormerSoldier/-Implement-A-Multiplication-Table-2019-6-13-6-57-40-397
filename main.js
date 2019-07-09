86a413892c596f8c5d4b8507ce2981f06af94561
TYG <793557590@qq.com>
2019-07-09 23:21:37 +0800

    feat: add function createMultiplyTable to get a multiply table in correct situation

--- main.js
+++ main.js
@@ -6,3 +6,19 @@ function isInRange(start, end){
     return start >= 1 && start <= 1000 && end >= 1 && end <= 1000;
 }
 
+function createMultiplyTable(startSmallerEqualEnd, inRange, start, end){
+    if(!startSmallerEqualEnd)
+        return null;
+    if(!inRange)
+        return 'Out Of Range';
+    var result = [];
+    var result_child = [];
+    for(let i = start; i <= end; i++){
+        for(let j = start; j <= i; j++){
+            result_child.push(i + '*' + j + '=' + (i*j));
+        }
+        result.push(result_child.join(' '));
+        result_child = [];
+    }
+    return result.join('\n');
+}
